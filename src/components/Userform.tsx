8a931c13817354923202278b8bdc3b91e0347f39
unknown <khizarahmad3333@gmail.com>
2024-10-15 23:01:25 +0500

    url

--- src/components/Userform.tsx
+++ src/components/Userform.tsx
@@ -37,8 +37,11 @@ const UserForm: React.FC = () => {
     };
 
     try {
-      const response = await fetch('https://crud-n02fv79kk-khizar3333s-projects.vercel.app/api/users', {
+      const response = await fetch('https://crud-cazowd5va-khizar3333s-projects.vercel.app', {
+      // const response = await fetch('http://localhost:5000/api/users', {
+
         method: 'POST',
+        
         headers: {
           'Content-Type': 'application/json',
         },
