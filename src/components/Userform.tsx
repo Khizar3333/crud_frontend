dd15b3b656b225e783d5da1a6e772ea3af4c6de2
unknown <khizarahmad3333@gmail.com>
2024-10-16 23:34:02 +0500

    fixed url

--- src/components/Userform.tsx
+++ src/components/Userform.tsx
@@ -37,7 +37,7 @@ const UserForm: React.FC = () => {
     };
 
     try {
-      const response = await fetch('https://crud-n44jxgxy7-khizar3333s-projects.vercel.app/ ', {
+      const response = await fetch('https://crud-zeta-seven.vercel.app/api/users ', {
       // const response = await fetch('http://localhost:5000/api/users', {
 
         method: 'POST',
